48a96e80c453a3a4ba8de6125e40bbe00c48658b
Forsinket <homero40210@gmail.com>
2020-12-03 05:25:59 -0500

    Se implementa movimiento al personaje y correccion de coliders en el mapa

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D control;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+
+
+    bool jump = false;
+
+    // Update is called once per frame
+    void Update(){
+
+        //El registro
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+
+        }
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        //Movimiento
+        control.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
